85a0e5508400d69517a9a609aba394e3485ad768
OrlandoLeyvaFontes <orlando.leyva245512@potros.itson.edu.mx>
2026-04-10 13:50:28 -0700

    Reapply "Pantalla estadisticas generales"
    This reverts commit 8f9fc5e69b4dc9dd72a5aa2247d9c501d368cfe1.

--- proyectoFinal_Equipo2/app/src/main/java/orlando/leyva/proyectofinal_equipo2/MainActivity.kt
+++ proyectoFinal_Equipo2/app/src/main/java/orlando/leyva/proyectofinal_equipo2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import orlando.leyva.proyectofinal_equipo2.ui.theme.ProyectoFinal_Equipo2Theme
+import orlando.leyva.proyectofinal_equipo2.ui.screens.PantallaEstadisticas
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +22,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProyectoFinal_Equipo2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        PantallaEstadisticas()
+                    }
                 }
             }
         }
